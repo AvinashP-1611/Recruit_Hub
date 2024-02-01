f3aa27935c33c4248b112645dce3eee8f59e0805
Avinash <134164909+AvinashP-1611@users.noreply.github.com>
2024-02-01 14:42:42 +0530

    Add files via upload

---
+++ Client/src/main/java/com/example/Backend_Recruit_Hub/BackendRecruitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Recruit_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRecruitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRecruitHubApplication.class, args);
+	}
+
+}
---
+++ Client/src/test/java/com/example/Backend_Recruit_Hub/BackendRecruitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Recruit_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRecruitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
