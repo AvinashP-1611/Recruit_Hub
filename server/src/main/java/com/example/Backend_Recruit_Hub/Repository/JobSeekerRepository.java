97810a3b92edddae86a7fd829c81c435be752fe1
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-02-10 02:20:40 +0530

    deleted server folder

--- server/src/main/java/com/example/Backend_Recruit_Hub/Repository/JobSeekerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Backend_Recruit_Hub.Repository;
-
-import com.example.Backend_Recruit_Hub.Models.JobSeeker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobSeekerRepository extends JpaRepository<JobSeeker,Integer> {
-}
